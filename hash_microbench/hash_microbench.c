8200b87f3518c2e06bd3f2fa7993623e064dfecf
yhsindev <yhsindev@gmail.com>
2026-05-19 10:41:50 +0800

    Add module parameters for hash microbenchmark

--- hash_microbench/hash_microbench.c
+++ hash_microbench/hash_microbench.c
@@ -2,9 +2,24 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+static int hash_type = 0;
+static int input_len = 64;
+static unsigned long iterations = 10000000;
+
+module_param(hash_type, int, 0444);
+MODULE_PARM_DESC(hash_type, "0=jhash2, 1=hsiphash, 2=siphash");
+
+module_param(input_len, int, 0444);
+MODULE_PARM_DESC(input_len, "Input length in bytes");
+
+module_param(iterations, ulong, 0444);
+MODULE_PARM_DESC(iterations, "Number of hash iterations");
+
 static int __init hash_microbench_init(void)
 {
 	pr_info("hash_microbench: loaded\n");
+	pr_info("hash_microbench: hash_type=%d input_len=%d iterations=%lu\n",
+		hash_type, input_len, iterations);
 	return 0;
 }
 
